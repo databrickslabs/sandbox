e169933107bd6d88c103645958d30f73078ceb6d
Serge Smertin <serge.smertin@databricks.com>
2024-02-03 20:19:33 +0100

    ......

--- acceptance/index.mjs
+++ acceptance/index.mjs
@@ -3,6 +3,7 @@ import fs from "fs";
 import Go from './wasm_exec_node.js'
 
 const go = new Go();
-const buf = fs.readFileSync(`${__dirname}/acceptance.wasm`);
+const buf = fs.readFileSync(`/home/runner/work/_actions/databrickslabs/sandbox/go/wasm/acceptance/acceptance.wasm`);
+// const buf = fs.readFileSync(`${__dirname}/acceptance.wasm`);
 const wasm = await WebAssembly.instantiate(new Uint8Array(buf), go.importObject);
 go.run(wasm.instance)
