a2a4f9053267625878b65ddf838c29064a1b0847
Serge Smertin <serge.smertin@databricks.com>
2024-02-08 19:44:32 +0000

    ..

--- acceptance/boilerplate/logger.go
+++ acceptance/boilerplate/logger.go
@@ -5,6 +5,6 @@ import "github.com/databricks/databricks-sdk-go/logger"
 func init() {
 	// TODO: integrate with GitHub Actions logging primitives
 	logger.DefaultLogger = &logger.SimpleLogger{
-		Level: logger.LevelDebug,
+		Level: logger.LevelInfo,
 	}
 }
