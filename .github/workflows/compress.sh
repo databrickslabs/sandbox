18728fb16572f163349e586788c622705abe11ba
Serge Smertin <serge.smertin@databricks.com>
2024-02-14 21:22:01 -0800

    Remove file after compression

--- .github/workflows/compress.sh
+++ .github/workflows/compress.sh
@@ -3,11 +3,10 @@
 for file in dist/*; do
     if [ -f "$file" ]; then
         filename=$(basename "$file" | cut -d. -f1)
-        zip -r "dist/$filename.zip" "$file" &
+        zip -r "dist/$filename.zip" "$file"
+        rm "dist/$file"
     fi
 done
-
-wait
 
 cd dist
 sha256sum * > SHA256SUMS
