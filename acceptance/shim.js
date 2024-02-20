067f922e846e14d0ed246e88a31c5cb9c953c60e
Serge Smertin <serge.smertin@databricks.com>
2024-02-20 10:48:27 +0100

    ..

--- acceptance/shim.js
+++ acceptance/shim.js
@@ -1,66 +1,32 @@
-// function chooseBinary() {
-//     // ...
-//     if (platform === 'linux' && arch === 'x64') {
-//         return `main-linux-amd64-${VERSION}`
-//     }
-//     // ...
-// }
-// const binary = chooseBinary()
-// const mainScript = `${__dirname}/${binary}`
-// TODO: try calling the result of GOOS=js GOARCH=wasm go build -o acceptance.wasm
+const version = 'v0.0.1';
+
 console.log('env', process.env)
 
-const https = require('https');
-const fs = require('fs');
+const { createWriteStream, chmodSync } = require('fs');
 const { createGunzip } = require('zlib');
 const { basename } = require('path');
 const { spawnSync } = require('child_process');
 const { pipeline } = require('stream');
 const { exit } = require('node:process');
+const { promisify } = require('util');
+const pipelineAsync = promisify(pipeline);
 
-
-// Function to download file
-function downloadFile(url) {
-    const filename = basename(url).split('.')[0]
-    const dest = fs.createWriteStream(filename);
-    const request = (url, resolve, reject) => {
-        https.get(url, (response) => {
-            if (response.statusCode >= 200 && response.statusCode < 300) {
-                pipeline(response, createGunzip(), dest, (err) => {
-                    if (err) {
-                        reject(err);
-                        return;
-                    }
-                    resolve(filename);
-                });
-            } else if (response.headers.location) {
-                request(response.headers.location, resolve, reject);
-            } else {
-                reject(new Error(`Failed to get '${url}' (${response.statusCode})`));
-            }
-        }).on('error', (err) => {
-            reject(err);
-        });
-    }
-    return new Promise((resolve, reject) => {
-        request(url, resolve, reject);
-    });
-}
-
-async function main() {
-    const version = 'v0.0.1'
+(async () => {
     const platform = process.platform == 'win32' ? 'windows' : process.platform;
     const arch = process.arch == 'x64' ? 'amd64' : process.arch;
     const artifact = `acceptance_${platform}_${arch}.gz`;
     const downloadUrl = `https://github.com/databrickslabs/sandbox/releases/download/acceptance/${version}/${artifact}`;
-    const binary = await downloadFile(downloadUrl);
-    fs.chmodSync(binary, '755')
-    const { status } = spawnSync(binary, [], { stdio: 'inherit' });
+    const filename = basename(downloadUrl).split('.')[0];
+    const dest = createWriteStream(filename);
+    const response = await fetch(downloadUrl);
+    if (!response.ok) {
+        throw new Error(`Failed to download file (status ${response.status}): ${response.statusText}`);
+    }   
+    await pipelineAsync(response.body, createGunzip(), dest)
+    chmodSync(filename, '755')
+    const { status } = spawnSync(`${process.cwd()}/${filename}`, [], { stdio: 'inherit' });
     exit(status);
-}
-
-main();
-
+})();
 
 // const { spawnSync } = require('child_process');
 // const { exit } = require('node:process');
