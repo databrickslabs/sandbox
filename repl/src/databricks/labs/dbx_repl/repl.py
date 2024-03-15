19869f84d697a696f8eadcf57babb018f711becc
Fabian Jakobs <fabian.jakobs@databricks.com>
2024-03-15 16:20:09 +0100

    add welcome message

--- repl/src/databricks/labs/dbx_repl/repl.py
+++ repl/src/databricks/labs/dbx_repl/repl.py
@@ -1,7 +1,24 @@
 from .context_handler import ContextHandler
+from prompt_toolkit import print_formatted_text, HTML
+from prompt_toolkit.styles import Style
+from prompt_toolkit.formatted_text import FormattedText
+
+def welcome():
+    
+    style = Style.from_dict({
+        'welcome': 'ansiyellow',
+        'code': '#ff0000'
+    })
+
+    print_formatted_text(HTML('''<welcome>Welcome to the Databricks REPL!
+ * Press Ctrl-D or Ctrl-C to exit
+ * Type <code>:python</code>, <code>:r</code>, <code>:sql</code>, or <code>:scala</code> to switch languages
+</welcome>'''), style=style)
+
 
 def repl(client, language, cluster_id):
 
+    welcome()
     handler = ContextHandler(client, cluster_id, language)
 
     while True:
