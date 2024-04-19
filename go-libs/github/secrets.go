ee03cb3f22c2432a32d41b983fbe1451ee0b967e
Serge Smertin <serge.smertin@databricks.com>
2024-04-19 19:19:08 +0200

    ..

--- go-libs/github/secrets.go
+++ go-libs/github/secrets.go
@@ -3,6 +3,7 @@ package github
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"fmt"
 	"time"
 
 	"golang.org/x/crypto/nacl/box"
@@ -29,11 +30,14 @@ type OverrideRepositorySecret struct {
 	KeyID          string `json:"key_id"`
 }
 
-func (c *GitHubClient) encryptSecretWithSodium(pk, clearText string) (string, error) {
+func (c *GitHubClient) encryptSecretWithSodium(pkBase64, clearText string) (string, error) {
 	// adapted from implementation by Sterling Hanenkamp (c) 2022
 	// See https://zostay.com/posts/2022/05/04/do-not-use-libsodium-with-go/
 	var pkBytes [32]byte
-	copy(pkBytes[:], pk)
+	_, err := base64.StdEncoding.Decode(pkBytes[:], []byte(pkBase64))
+	if err != nil {
+		return "", fmt.Errorf("public key: %w", err)
+	}
 	raw := []byte(clearText)
 	out := make([]byte, 0, len(raw)+box.Overhead+len(pkBytes))
 	encryptedRaw, err := box.SealAnonymous(out, raw, &pkBytes, rand.Reader)
