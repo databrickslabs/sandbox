6f5d31afe2ca75633143506bbd87392165851726
robert.whiffin <robert.whiffin@databricks.com>
2024-10-29 10:55:52 +0000

    update llm calls to use openai to avoid timeout

--- sql_migration_assistant/app/llm.py
+++ sql_migration_assistant/app/llm.py
@@ -5,8 +5,8 @@ from databricks.sdk.service.serving import ChatMessage, ChatMessageRole
 
 
 class LLMCalls:
-    def __init__(self, foundation_llm_name):
-        self.w = WorkspaceClient()
+    def __init__(self, openai_client, foundation_llm_name):
+        self.o = openai_client
         self.foundation_llm_name = foundation_llm_name
 
     def call_llm(self, messages, max_tokens, temperature):
@@ -26,26 +26,14 @@ class LLMCalls:
         # check to make sure temperature is between 0.0 and 1.0
         if temperature < 0.0 or temperature > 1.0:
             raise gr.Error("Temperature must be between 0.0 and 1.0")
-        response = self.w.serving_endpoints.query(
-            name=self.foundation_llm_name,
+        response = self.o.chat.completions.create(
+            model=self.foundation_llm_name,
             max_tokens=max_tokens,
             messages=messages,
             temperature=temperature,
         )
         message = response.choices[0].message.content
         return message
-
-    def convert_chat_to_llm_input(self, system_prompt, chat):
-        # Convert the chat list of lists to the required format for the LLM
-        messages = [ChatMessage(role=ChatMessageRole.SYSTEM, content=system_prompt)]
-        for q, a in chat:
-            messages.extend(
-                [
-                    ChatMessage(role=ChatMessageRole.USER, content=q),
-                    ChatMessage(role=ChatMessageRole.ASSISTANT, content=a),
-                ]
-            )
-        return messages
 
     ################################################################################
     # FUNCTION FOR TRANSLATING CODE
@@ -55,30 +43,22 @@ class LLMCalls:
 
     def llm_translate(self, system_prompt, input_code, max_tokens, temperature):
         messages = [
-            ChatMessage(role=ChatMessageRole.SYSTEM, content=system_prompt),
-            ChatMessage(role=ChatMessageRole.USER, content=input_code),
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": input_code}
         ]
 
         # call the LLM end point.
         llm_answer = self.call_llm(
             messages=messages, max_tokens=max_tokens, temperature=temperature
         )
-        # Extract the code from in between the triple backticks (```), since LLM often prints the code like this.
-        # Also removes the 'sql' prefix always added by the LLM.
-        translation = llm_answer  # .split("Final answer:\n")[1].replace(">>", "").replace("<<", "")
-        return translation
 
-    def llm_chat(self, system_prompt, query, chat_history):
-        messages = self.convert_chat_to_llm_input(system_prompt, chat_history)
-        messages.append(ChatMessage(role=ChatMessageRole.USER, content=query))
-        # call the LLM end point.
-        llm_answer = self.call_llm(messages=messages)
-        return llm_answer
+        translation = llm_answer
+        return translation
 
     def llm_intent(self, system_prompt, input_code, max_tokens, temperature):
         messages = [
-            ChatMessage(role=ChatMessageRole.SYSTEM, content=system_prompt),
-            ChatMessage(role=ChatMessageRole.USER, content=input_code),
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": input_code}
         ]
 
         # call the LLM end point.
