c8e5d0eb165a443b342ec49680741741096dfe49
chi-yang-db <chi.yang@databricks.com>
2025-08-13 16:50:13 -0700

    Enable bundle target in tool scripts

--- filepush/filepush-template/template/{{.schema_name}}/tools/upload_to_volume.sh
+++ filepush/filepush-template/template/{{.schema_name}}/tools/upload_to_volume.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
-. $(dirname $0)/env.sh
+usage() {
+    echo "Usage: $(basename $0) <table_name> <local_file_path> [--target=dev|prod]"
+}
+export -f usage
+. $(dirname $0)/env.sh $@
 if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "Usage: $0 <table_name> <local_file_path>"
+  usage
   exit 1
 fi
 databricks fs mkdir dbfs:${FILEPUSH_VOLUME_PATH}$1/
