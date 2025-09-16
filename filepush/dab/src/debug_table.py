337417dc76cc284a2e4f603dfddd8303b58038a6
chi-yang-db <chi.yang@databricks.com>
2025-09-15 21:51:53 -0700

    Add config manager and a working debug notebook

--- filepush/dab/src/debug_table.py
+++ filepush/dab/src/debug_table.py
@@ -20,12 +20,24 @@ with open(table_configs_path, "r") as f:
 catalog_name = configs["catalog_name"]
 schema_name = configs["schema_name"]
 table_name = dbutils.widgets.get("table_name")
+assert table_name, "Please provide a table name"
 table_volume_path_data = configs["volume_path_data"] + f"/{table_name}"
 
 # Locate table config
-matches = [table_config for table_config in table_configs if table_config.get("name") == "dummy"]
+matches = [table_config for table_config in table_configs if table_config.get("name") == table_name]
 assert len(matches) == 1, f"Expect exactly 1 config for table `{table_name}`. Found {len(matches)}. Please fix the config file."
 table_config = matches[0]
 
 print(f"Table Volume Path: {table_volume_path_data}")
 print(f"Table Config: {table_config}")
+
+# COMMAND ----------
+
+import tempfile
+from utils import configmanager
+
+with tempfile.TemporaryDirectory() as tmpdir:
+    reader = spark.readStream.format("cloudFiles")
+    reader = configmanager.apply_table_config(reader, table_config)
+    reader.option("cloudFiles.schemaLocation", tmpdir)
+    display(reader.load(table_volume_path_data))
