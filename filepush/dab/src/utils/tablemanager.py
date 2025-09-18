29b8609706890d89f689c75721f51e7a09594eae
chi-yang-db <chi.yang@databricks.com>
2025-09-18 11:14:00 -0700

    Working format manager

--- filepush/dab/src/utils/tablemanager.py
+++ filepush/dab/src/utils/tablemanager.py
@@ -1,25 +1,21 @@
 import os
 import json
 from . import envmanager
+from . import formatmanager
 from pyspark.sql.streaming import DataStreamReader
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.errors.platform import NotFound
 
-def validate_config(config: dict):
-  pass
-
 def get_table_volume_path(table_name: str) -> str:
-  # Initialize workspace client
   ws = WorkspaceClient()
   table_volume_path_data = os.path.join(envmanager.get_config()["volume_path_data"], table_name)
   try:
     ws.files.get_directory_metadata(table_volume_path_data)
   except NotFound:
-    assert False, f"Table data path not found for table `{table_name}`. Have you run `databricks bundle run configuration_job`?"
+    raise RuntimeError(f"Table data path not found for table `{table_name}`. Have you run `databricks bundle run configuration_job`?")
   return table_volume_path_data
 
 def has_data_file(table_name: str) -> bool:
-  # Initialize workspace client
   ws = WorkspaceClient()
   table_volume_path_data = get_table_volume_path(table_name)
   try:
@@ -30,25 +26,29 @@ def has_data_file(table_name: str) -> bool:
   return True
 
 def is_table_created(table_name: str) -> bool:
-  # Initialize workspace client
   ws = WorkspaceClient()
   return ws.tables.exists(full_name=f"{envmanager.get_config()['catalog_name']}.{envmanager.get_config()['schema_name']}.{table_name}").table_exists
 
 def get_configs() -> list:
   json_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "configs", "tables.json")
-  assert os.path.exists(json_path), f"Missing table configs file: {json_path}. Please following README.md to create one, deploy and run configuration_job." 
+  if not os.path.exists(json_path):
+    raise RuntimeError(f"Missing table configs file: {json_path}. Please following README.md to create one, deploy and run configuration_job.")
   with open(json_path, "r") as f:
     configs = json.load(f)
   return configs
 
 def apply_table_config(reader: DataStreamReader, table_config: dict) -> DataStreamReader:
   fmt = table_config.get("format")
-  assert fmt is not None, f"format is required for table {table_config.get('name')}"
-  reader = reader.option("cloudFiles.format", fmt)
+  if fmt is None:
+    raise ValueError(f"format is required for table {table_config.get('name')}")
 
-  # format-specific options
-  fmt_opts = table_config.get("format_options", {})
-  for k, v in fmt_opts.items():
+  # format-specific options from user input
+  user_fmt_opts = table_config.get("format_options", {})
+  # validate and get the final modified options
+  final_fmt_opts = formatmanager.get_format_manager(fmt).get_modified_options(user_fmt_opts)
+
+  reader = reader.format("cloudFiles").option("cloudFiles.format", fmt)
+  for k, v in final_fmt_opts.items():
     reader = reader.option(k, v)
   
   # schema hints
