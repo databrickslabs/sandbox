b0beb24988dd763e57604d40e9a537247bd1b253
chi-yang-db <chi.yang@databricks.com>
2025-09-19 16:47:56 -0700

    Clean up workspace

--- filepush/filepush-template/template/{{.schema_name}}/tools/trigger_refresh.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-usage() {
-    echo "Usage: $(basename $0) [--target=dev|prod]"
-}
-export -f usage
-. $(dirname $0)/env.sh $@
-databricks bundle run $FILEPUSH_JOB_NAME -t $BUNDLE_TARGET
