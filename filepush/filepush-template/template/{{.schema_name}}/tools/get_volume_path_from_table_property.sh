257937185f2dbeb9a2f1d413d2bb106e0a84318b
chi-yang-db <chi.yang@databricks.com>
2025-09-19 16:47:56 -0700

    Clean up workspace

--- filepush/filepush-template/template/{{.schema_name}}/tools/get_volume_path_from_table_property.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-usage() {
-    echo "Usage: $(basename $0) <table_name> [--target=dev|prod]"
-}
-export -f usage
-. $(dirname $0)/env.sh $@
-databricks tables get ${FILEPUSH_CATALOG_NAME}.${FILEPUSH_SCHEMA_NAME}.$1 -t $BUNDLE_TARGET --output json | jq '.properties["filepush.volume_path"]'
